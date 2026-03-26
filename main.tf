51d3a5195dba64bb77ebecba121c255693698611
CarlosCruz-117 <ccdeh@hotmail.com>
2026-03-26 20:13:40 +0100

    adds api gw request validation

--- main.tf
+++ main.tf
@@ -1,26 +1,29 @@
-resource "aws_cloudwatch_event_bus" "custom_bus" {
-  name = "${local.name_prefix}-bus"
-}
-resource "aws_api_gateway_integration" "eventbridge" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.post.http_method
-  type                    = "AWS"
-  integration_http_method = "POST"
-  uri = "arn:aws:apigateway:${var.region}:events:action/PutEvents"
-credentials = aws_iam_role.apigw_role.arn
-request_templates = {
-    "application/json" = <<EOF
-{
-  "Entries": [
-    {
-      "Source": "custom.api",
-      "DetailType": "customEvent",
-      "Detail": "$util.escapeJavaScript($input.body)",
-      "EventBusName": "${aws_cloudwatch_event_bus.custom_bus.name}"
+resource "aws_api_gateway_model" "request_model" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "RequestModel"
+content_type = "application/json"
+schema = jsonencode({
+    type = "object",
+    required = ["type", "payload"],
+    properties = {
+      type = { type = "string" },
+      payload = { type = "object" }
     }
-  ]
+  })
 }
-EOF
+resource "aws_api_gateway_request_validator" "validator" {
+  rest_api_id                 = aws_api_gateway_rest_api.api.id
+  name                        = "validator"
+  validate_request_body       = true
+  validate_request_parameters = false
+}
+resource "aws_api_gateway_method" "post" {
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.resource.id
+  http_method      = "POST"
+  authorization    = "NONE"
+  request_models = {
+    "application/json" = aws_api_gateway_model.request_model.name
   }
+  request_validator_id = aws_api_gateway_request_validator.validator.id
 }
