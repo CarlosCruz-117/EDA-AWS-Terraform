8fbf00ca6e3b229d194a882797aaf81bcf288ec1
CarlosCruz-117 <ccdeh@hotmail.com>
2026-03-26 20:06:41 +0100

    refactor: direct service proxy integration + role API GW

--- main.tf
+++ main.tf
@@ -1,37 +1,26 @@
-provider "aws" {
-  region = var.region
+resource "aws_cloudwatch_event_bus" "custom_bus" {
+  name = "${local.name_prefix}-bus"
 }
-resource "aws_sqs_queue" "main_queue" {
-  name = "${local.name_prefix}-queue"
-tags = local.common_tags
-}
-resource "aws_lambda_function" "api_handler" {
-  function_name = "${local.name_prefix}-lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-filename = "lambda.zip"
-tags = local.common_tags
-}
-resource "aws_api_gateway_rest_api" "api" {
-  name = "${local.name_prefix}-api"
-}
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "event"
-}
-resource "aws_api_gateway_method" "post" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "eventbridge" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.post.http_method
+  type                    = "AWS"
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.api_handler.invoke_arn
+  uri = "arn:aws:apigateway:${var.region}:events:action/PutEvents"
+credentials = aws_iam_role.apigw_role.arn
+request_templates = {
+    "application/json" = <<EOF
+{
+  "Entries": [
+    {
+      "Source": "custom.api",
+      "DetailType": "customEvent",
+      "Detail": "$util.escapeJavaScript($input.body)",
+      "EventBusName": "${aws_cloudwatch_event_bus.custom_bus.name}"
+    }
+  ]
+}
+EOF
+  }
 }
