b334d17fe9de89dd0625c027c6672fa402c25d0f
CarlosCruz-117 <ccdeh@hotmail.com>
2026-03-29 14:06:04 +0200

    security enhancement: WAF

--- main.tf
+++ main.tf
@@ -38,3 +38,41 @@ event_pattern = jsonencode({
     }
   })
 }
+
+resource "aws_wafv2_web_acl" "api_waf" {
+  name  = "${local.name_prefix}-waf"
+  scope = "REGIONAL"
+default_action {
+    allow {}
+  }
+rule {
+    name     = "rate-limit"
+    priority = 1
+action {
+      block {}
+    }
+statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+      }
+    }
+visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "rateLimit"
+      sampled_requests_enabled   = true
+    }
+  }
+visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "waf"
+    sampled_requests_enabled   = true
+  }
+}
+
+resource "aws_wafv2_web_acl_association" "api" {
+  resource_arn = aws_api_gateway_stage.stage.arn
+  web_acl_arn  = aws_wafv2_web_acl.api_waf.arn
+}
+
+
